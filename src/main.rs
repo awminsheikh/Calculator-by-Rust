94becb42ed0eabef34bd7978c61701a7c942be3d
aminsheikh00 <amin.sheikhazimi221@gmail.com>
2024-10-24 21:08:22 +0330

    Virsion 1.0 just simple operations and calculations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::io;
 fn main() {
-    println!("Hello , World!");
+    println!("❤  Welcome to calculator , When you'r finished you can exit by typung exit ❤");
+    loop {
+        // Read user input
+        let mut input = String::new();
+        println!("Enter a calculation");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        //Thrim whitespace and check for exist
+        let input = input.trim();
+        if input.eq_ignore_ascii_case("exit") {
+            println!("Calculator existed , Bye❤.");
+            break;
+        }
+
+        //Parse the input
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.len() != 3 {
+            println!("Invalid input . Please enter in the format: <number> <operator> <number>");
+            continue; // start the loop again to get valid input from user
+        }
+
+        //extract numbers and operator
+        let num1: f64 = match parts[0].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Invalid number: {}", parts[0]);
+                continue; // start the loop again to get valid input from user
+            }
+        };
+        let operator = parts[1];
+        let num2: f64 = match parts[2].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Invalid number: {}", parts[2]);
+                continue;
+            }
+        };
+
+        //Perform the Operation
+        let result = match operator {
+            "+" => num1 + num2,
+            "-" => num1 - num2,
+            "*" => num1 * num2,
+            "/" => {
+                if num2 == 0.0 {
+                    println!("Error: Division by zero is not allowed!");
+                    continue;
+                }
+                num1 / num2
+            }
+
+            _ => {
+                println!("Ibvalid operator: {}", operator);
+                continue;
+            }
+        };
+        println!("Result: {}", result);
+    }
 }
